d5f11a5d16348021504f3354d4bf6cbe384d217c
Anchit Bansal <anchitbansal11@gmail.com>
2024-10-23 02:35:10 +0530

    Model Trainer

--- src/exception.py
+++ src/exception.py
@@ -4,7 +4,7 @@ from src.logger import logging
 def error_message_detail(error, error_detail:sys):
     _,_,exe_tb = error_detail.exc_info()
     file_name = exe_tb.tb_frame.f_code.co_filename
-    error_message = "Error occured in python script name [{0}] line number [{1}] error messaeg [{2}]".format(
+    error_message = "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
         file_name, exe_tb.tb_lineno, str(error)
     )
     
